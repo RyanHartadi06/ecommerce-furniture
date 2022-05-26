9ecf6dbadadc5e7ea2554822865c84beca005874
Abdul Masjit <abdulmasjits@gmail.com>
2022-05-26 21:32:34 +0700

    Endpoint Add Cart, List Cart

---
+++ application/controllers/Order.php
@@ -0,0 +1,48 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+class Order extends CI_Controller {
+  private $nama_menu  = "Home";     
+  public function __construct()
+  {
+    parent::__construct();
+    $this->apl = get_apl();
+    $this->load->model('Order_m');
+  }
+
+  public function get_cart()
+  {
+    
+  }
+
+  public function add_cart()
+  {
+    $id_role = $this->session->userdata('auth_id_user');
+    $id_produk = $this->input->post('id_produk');
+    $qty = $this->input->post('qty');
+
+    $is_login = $this->session->userdata('auth_is_login');
+    if($is_login){
+      $id = $this->uuid->v4(false);  
+      $data_object = array(
+        'id'=>$id,
+        'id_user'=>$id_user,
+        'id_produk'=>$id_produk,
+        'qty'=>$qty,
+        'is_checkout'=>'0',
+        'created_at'=>date('Y-m-d H:i:s')
+      );
+      $this->db->insert('chart', $data_object);
+  
+      $response['success'] = TRUE;
+      $response['message'] = "Produk berhasil ditambahkan ke dalam keranjang";
+    }else{
+      $response['success'] = TRUE;
+      $response['message'] = "Maaf, silahkan login terlebih dahulu";
+    }
+
+    echo json_encode($response);   
+  }
+      
+}
+
+/* End of file Order.php */
---
+++ application/models/Order_m.php
@@ -0,0 +1,20 @@
+<?php 
+    defined('BASEPATH') OR exit('No direct script access allowed');
+    class Order_m extends CI_Model {
+
+      function get_list_cart($id_user){
+        $query = $this->db->query("
+            SELECT c.*, d.kode, d.nama, d.harga, (
+              SELECT foto FROM m_produk_image
+              WHERE id_produk = c.id_produk
+              ORDER BY created_at asc
+              LIMIT 1
+            ) AS foto FROM cart c
+            LEFT JOIN m_produk d ON c.id_produk = d.id
+            WHERE c.id_user = '$id_user'
+            AND c.is_checkout = 0        
+        ");
+        return $query;
+      }
+    }
+?>
