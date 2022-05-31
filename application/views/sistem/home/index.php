a267e17e66f25187882864e75c7fb73ff3c1a808
Abdul Masjit <abdulmasjits@gmail.com>
2022-05-31 07:00:06 +0700

    session login

--- application/views/sistem/home/index.php
+++ application/views/sistem/home/index.php
@@ -2,7 +2,7 @@
   <div class="col-md-12 grid-margin">
     <div class="row">
       <div class="col-12 col-xl-8 mb-4 mb-xl-0">
-        <h3 class="font-weight-bold">Hallo Aamir</h3>
+        <h3 class="font-weight-bold">Hallo <?= $this->session->userdata("auth_nama_user"); ?></h3>
         <h6 class="font-weight-normal mb-0">Selamat datang di aplikasi E-Commerce Furniture.</span></h6>
       </div>
       <div class="col-12 col-xl-4">
